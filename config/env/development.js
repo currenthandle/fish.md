6b190d8f2d6269d350c961d3faa9a99812ee1d21
leptone <leptone55@gmail.com>
2015-12-02 10:43:56 -0800

    changed mongo URI to mongoLab's (heroku)

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,5 @@
 module.exports = {
-	db: 'mongodb://localhost/fish-doctor',
+	//db: 'mongodb://localhost/fish-doctor',
+	db: 'mongodb://<dbuser>:<dbpassword>@ds059694.mongolab.com:59694/heroku_2kf6v4sk',
 	sessionSecret: 'developmentSessionSecret'
 }
